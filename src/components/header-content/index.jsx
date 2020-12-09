b4e809c8eaa637fae2658375296efc82c4b1dd4d
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2020-12-09 17:13:01 +0200

    axios, perpare icons and static images and also the fonts

--- src/components/header-content/index.jsx
+++ src/components/header-content/index.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import "./index.css";
+import PropTypes from "prop-types";
+import Navbar from "../navbar/index";
+import HeaderBanner from "../header-banner/index";
+import Button from "../UI/Button/index";
+import BookNow from "../../assets/icons/book now.svg";
+import ScrollButtonSrc from "../../assets/icons/scroll.svg";
+import ScrollButton from "../UI/scroll-button/index";
 
 export default function HeaderContent(props) {
-  return <div className="header-content" />;
+  const { scrollTo } = props;
+  return (
+    <div className="header-content">
+      <Navbar />
+      <HeaderBanner />
+      <div className="header-footer">
+        <Button text="BOOK NOW" Icon={BookNow} size="normal" />
+        <ScrollButton
+          text="Scroll"
+          icon={ScrollButtonSrc}
+          scrollTo={scrollTo}
+        />
+      </div>
+    </div>
+  );
 }
+HeaderContent.propTypes = {
+  scrollTo: PropTypes.elementType.isRequired,
+};
---
+++ src/images.js
@@ -0,0 +1,5 @@
+import HomeBG from "./assets/bg-images/gabriel.png";
+import Facilities from "./assets/bg-images/palm-trees.png";
+import RoomsBG from "./assets/bg-images/apartment.png";
+
+export { HomeBG, Facilities, RoomsBG };
