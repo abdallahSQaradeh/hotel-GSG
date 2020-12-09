9c22aa06c624aa859bd0572ff8a37285f9e49621
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2020-12-09 17:33:48 +0200

    add body component and its subcombonents

---
+++ src/components/body-items/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BodyItems() {
+  return (
+    <div className="body-items">
+      <div>item1</div>
+      <div>item-2</div>
+    </div>
+  );
+}
